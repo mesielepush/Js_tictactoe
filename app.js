d2ef1663c743011a91d463d4e4f523df179a542d
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-26 17:28:15 -0500

    rewritting board
    Now instead of being render from every html element just rendedr the text inside each span

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
 let board = ['','','','','','','','','']
 
-const theBoard = document.getElementById('board_container');
+
 
 const Player = (name, token) => ({
     name,
     token,
 });
 
-function makeAMove(index,token) {
-    board[index] = token
-}
-
 function render() {
-    theBoard.innerHTML = '';
-    
-    const template = `
-        
-            <div class="board">
-                <div class="row">
-                    <div class="square" id='square_1'><span>${board[0]}</span></div>
-                    <div class="square" id='square_2'><span>${board[1]}</span></div>
-                    <div class="square" id='square_3'><span>${board[2]}</span></div>
-                </div>
-                <div class="row">
-                    <div class="square" id='square_4'><span>${board[3]}</span></div>
-                    <div class="square" id='square_5'><span>${board[4]}</span></div>
-                    <div class="square" id='square_6'><span>${board[5]}</span></div>
-                </div>
-                <div class="row">
-                    <div class="square" id='square_7'><span>${board[6]}</span></div>
-                    <div class="square" id='square_8'><span>${board[7]}</span></div>
-                    <div class="square" id='square_9'><span>${board[8]}</span></div>
-                </div>            
-            </div>
-            
-        
-        `;
-        theBoard.innerHTML += template;
-    
+    board.forEach((element, index) => {
+        const node = document.getElementById(`square_${index+1}`);
+        console.log(index)
+        const textnode = document.createTextNode(element);
+        node.addEventListener('click', () => {
+            mainGame().move(index);
+        });
+        node.appendChild(textnode);
+      });
   }
   const mainGame = () => {
     let player1;
@@ -91,9 +70,10 @@ function render() {
       board = ['', '', '', '', '', '', '', '', ''];
       set_init(name, token, name2, token2);
       document.getElementById('current').innerHTML =`It's ${current.name} turn`;
-      render()
+      render();
       
     };
+    
     return {
       move,
       set_init,
@@ -104,17 +84,6 @@ function render() {
   document.getElementById('start').addEventListener('click', () => {
     const game = mainGame();
     game.startGame();
-    document.getElementById('square_1').addEventListener('click',move(0));
-    document.getElementById('square_2').addEventListener('click',move(1));
-    document.getElementById('square_3').addEventListener('click',move(2));
-    document.getElementById('square_4').addEventListener('click',move(3));
-    document.getElementById('square_5').addEventListener('click',move(4));
-    document.getElementById('square_6').addEventListener('click',move(5));
-    document.getElementById('square_7').addEventListener('click',move(6));
-    document.getElementById('square_8').addEventListener('click',move(7));
-    document.getElementById('square_9').addEventListener('click',move(8));
-    render()
+    console.log(board)
   });
-  
-render();
   
