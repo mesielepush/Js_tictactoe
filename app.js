7b08175876052169a00967c47989d1c9614b3c10
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-26 16:15:31 -0500

    main logic almost working just not really

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const board = ['','','','','','','','','']
+let board = ['','','','','','','','','']
 
 const theBoard = document.getElementById('board_container');
+
+const Player = (name, token) => ({
+    name,
+    token,
+});
+
+function makeAMove(index,token) {
+    board[index] = token
+}
 
 function render() {
     theBoard.innerHTML = '';
@@ -30,5 +39,67 @@ function render() {
         theBoard.innerHTML += template;
     
   }
-
-  render()
+  const mainGame = () => {
+    let player1;
+    let player2;
+    let current;
+    let gameEnd;
+    const set_init = (name, token, name2, token2) => {
+      player1 = Player(name, token);
+      player2 = Player(name2, token2);
+      current = player1;
+    };
+    const horizontal = () => ((board[0] !== '') && (board[0] === board[1] && board[1] === board[2]))
+                            || ((board[3] !== '') && (board[3] === board[4] && board[4] === board[5]))
+                            || ((board[6] !== '') && (board[6] === board[7] && board[7] === board[8]));
+  
+    const vertical = () => ((board[0] !== '') && (board[0] === board[3]) && (board[0] === board[6]))
+                            || ((board[1] !== '') && (board[1] === board[4] && board[4] === board[7]))
+                            || ((board[2] !== '') && (board[2] === board[5] && board[5] === board[8]));
+  
+    const diagonal = () => ((board[0] !== '') && (board[0] === board[4] && board[4] === board[8]))
+                            || ((board[2] !== '') && (board[2] === board[4] && board[4] === board[6]));
+  
+    const is_winner = () => horizontal() || vertical() || diagonal();
+    const draw = () => board.filter(x => x === '').length === 0;
+    const move = (index) => {
+      if (board[index] !== '') {
+        return;
+      }
+      if (!gameEnd) {
+        board[index] = current.token;
+        render();
+      }
+      if (is_winner()) {
+        gameEnd = true;
+        document.getElementById('current').innerHTML = `${current.name} Has Won. Congratulations!`;
+      } else if (draw()) {
+        gameEnd = true;
+        document.getElementById('current').innerHTML = "...It's a Draw You're both losers smh...";
+      } else {
+        current = current === player1 ? player2 : player1;
+        document.getElementById('current').innerHTML = `It's ${current.name} turn`;
+      }
+    };
+    const startGame = () => {
+      gameEnd = false;
+      const name = document.getElementById('player1').value;
+      const name2 = document.getElementById('player2').value;
+      const token = document.getElementById('token1').value;
+      const token2 = document.getElementById('token2').value;
+      board = ['', '', '', '', '', '', '', '', ''];
+      set_init(name, token, name2, token2);
+      document.getElementById('current').innerHTML =`It's ${current.name} turn`;
+      render();
+    };
+    return {
+      move,
+      set_init,
+      startGame,
+    };
+  };
+  
+  document.getElementById('start').addEventListener('click', () => {
+    console.log('lkasjlkdj');
+    mainGame().startGame();
+  });
